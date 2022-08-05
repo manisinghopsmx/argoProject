c0ae7f8bee3eca1aa2ef9188a01eceaa0f4eb5b1
Mani Singh <mani.singh@opsmx.io>
2022-08-05 09:35:23 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/argoProject/ArgoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.argoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/argoProject/ArgoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.argoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
